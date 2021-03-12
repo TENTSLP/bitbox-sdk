53e93e9144dd75fb85efeedc35a2e96b18fe9865
ciripel <nedelcu.alexandru@yahoo.com>
2021-03-12 11:36:59 +0200

    Tent Changes

--- lib/Socket.ts
+++ lib/Socket.ts
@@ -10,7 +10,7 @@ export class Socket {
       if (config.restURL) {
         this.socket = io(`${config.restURL}`)
       } else {
-        const restURL = "https://rest.bitcoin.com"
+        const restURL = "https://rest.tent.app"
         this.socket = io(`${restURL}`)
       }
 
